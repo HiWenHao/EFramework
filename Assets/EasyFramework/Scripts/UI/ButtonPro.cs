da5891815f364b9312bc31ddac6421fd31973cc9
锁魂手 <841298494@qq.com>
2023-01-28 15:28:34 +0800

    Changed something

--- Assets/EasyFramework/Scripts/UI/ButtonPro.cs
+++ Assets/EasyFramework/Scripts/UI/ButtonPro.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdd59b4e4927708f70f8a1870f056cac824c45b64d452d6c53aa246e2ec7de8f
-size 5269
+oid sha256:b33ee97cb55911596b86ebfa7845598e03f85e23efe90da68db77549c74f405d
+size 5314
---
+++ Assets/EasyFramework/Scripts/UI/ScrollRectPro.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aa9ac03c2e62fd527bf380262da21c91f285c9b250fc194857572c96094c9918
+size 2926
