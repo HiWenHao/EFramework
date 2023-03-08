7b4d91a64dd6a260fdbf9c525bfef72758c3c3dd
锁魂手 <841298494@qq.com>
2023-03-08 18:52:59 +0800

    Added font

--- Assets/EasyFramework/Scripts/Managers/EF.Mono.cs
+++ Assets/EasyFramework/Scripts/Managers/EF.Mono.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8156d74a55f361a4981291bf2145a020b8b3b38af22968dfdb459ea5379dfe88
-size 5364
+oid sha256:d2f0c7a64b213ce104d6747ffdd743b6405ba9dcfdf4a8c26692335a93e79187
+size 5283
