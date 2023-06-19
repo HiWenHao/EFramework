d22e94b66d9abf35753efef81306730ca9842cbb
锁魂手 <841298494@qq.com>
2023-06-19 14:48:02 +0800

    Changed something

--- Assets/EasyFramework/ThirdPartyAssets/YooAsset/Editor/AssetBundleBuilder/BuildSystem/BuildLogger.cs
+++ Assets/EasyFramework/ThirdPartyAssets/YooAsset/Editor/AssetBundleBuilder/BuildSystem/BuildLogger.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f38c84af6d1e187e8a27b39c1e578451f80ea1dadb0b76d4d6c7dbc82f6f161
-size 546
+oid sha256:288d45f491d0740c52228547d2aa794da455be7abc5b8a9c4f57087a931eb1ac
+size 609
