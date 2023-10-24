698203ba4f14a8ca6adb5d1a9115f50740a447d8
锁魂手 <841298494@qq.com>
2023-10-24 09:36:15 +0800

    Update PatchManager.cs

--- Assets/EasyFramework/Scripts/Runtime/Managers/PatchManager.cs
+++ Assets/EasyFramework/Scripts/Runtime/Managers/PatchManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7215777cbea23ff779f0b31c009c04ba612594713a23122e3a29ecad71a5f918
-size 12310
+oid sha256:6de99fd699baa3ccfe3fa23e91344dfe112fd506192765b43535f6c8e21909f9
+size 12312
