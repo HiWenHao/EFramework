994c417de6e3a30ded9b9ff4858ac63cfa618de9
锁魂手 <841298494@qq.com>
2024-02-02 18:10:29 +0800

    Added  system information show.

--- Assets/EasyFramework/Scripts/Editor/Config/EF.LanguageConfig.cs
+++ Assets/EasyFramework/Scripts/Editor/Config/EF.LanguageConfig.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfce421448a6d612bf212198f0c00d8f43cdefcda7eacd315a713d7b71f2e4ce
-size 13661
+oid sha256:d88c624c5f9eb15e880d6616c3fb0cc197691dc36b09ca2ee8e314e46818ad62
+size 13882
