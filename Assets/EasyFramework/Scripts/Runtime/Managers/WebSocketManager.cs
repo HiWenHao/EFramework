ecbecb045b17cc8a8fd1bc25087195d9b2aee48c
锁魂手 <841298494@qq.com>
2024-02-07 17:44:27 +0800

    Changed the font path

--- Assets/EasyFramework/Scripts/Runtime/Managers/WebSocketManager.cs
+++ Assets/EasyFramework/Scripts/Runtime/Managers/WebSocketManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f23c0b5a8fcd90e652ea567b86ea5ae0a59e745fdc3d259e278070ff39a1429
-size 12029
+oid sha256:f3dc2092ac411141bc4c01d50bc22e29ead690b242b8393b0c81c770ee091c5d
+size 12100
--- Assets/ExampleGame/Scripts/GameStart.cs
+++ Assets/ExampleGame/Scripts/GameStart.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:385acd9b38251a9b86c80107cccfe1e5690334dc920aa2ad37e81919130d048b
-size 4577
+oid sha256:f706510ccb48ae604b3bdd7736ec14c21d9227905aeed4709097f9eab35a31b4
+size 4671
