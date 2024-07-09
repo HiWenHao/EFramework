0e0a3c6a621d7ccb08e4c7ad58968f2b2f526b6b
锁魂手 <841298494@qq.com>
2024-07-09 17:54:45 +0800

    Update PatchManager.cs

--- EF_Unity/Assets/EasyFramework/Scripts/Runtime/Managers/PatchManager.cs
+++ EF_Unity/Assets/EasyFramework/Scripts/Runtime/Managers/PatchManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5786eeea7a2be7f785fa458bb07acd6386629ffcd275d96176e8e7733bb8e9e
-size 18269
+oid sha256:a7d49ca23045d71c0eef8853e8d6845a5d24d14bc468657ceb371fed33913003
+size 18271
