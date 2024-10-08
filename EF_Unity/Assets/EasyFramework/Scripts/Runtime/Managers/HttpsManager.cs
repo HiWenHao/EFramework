97309deb31508520d499054e86dbb78e9d4af37c
HiWenHao <45863791+HiWenHao@users.noreply.github.com>
2024-10-08 14:49:48 +0800

    Changed http manager

--- EF_Unity/Assets/EasyFramework/Scripts/Runtime/Managers/HttpsManager.cs
+++ EF_Unity/Assets/EasyFramework/Scripts/Runtime/Managers/HttpsManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9ea0bd36c7e16bf13851e919f7f1b80c53959bdf224b945305a754436757df9
-size 6284
+oid sha256:e3360d5c171a8119829dc49637779686e2d9f6f597cee21e42c75b2e441b2de5
+size 6203
