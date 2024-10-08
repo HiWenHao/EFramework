02d35a3533c7464d7d88fada47baa75af0710d6b
HiWenHao <45863791+HiWenHao@users.noreply.github.com>
2024-10-08 15:43:50 +0800

    Update PatchManager.cs

--- EF_Unity/Assets/EasyFramework/Scripts/Runtime/Managers/PatchManager.cs
+++ EF_Unity/Assets/EasyFramework/Scripts/Runtime/Managers/PatchManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:229f67e76176edd3cc3f7f5d15ae5ae5bb48e31ce8e93f1a6c7d902a1526c821
-size 19173
+oid sha256:5e9502d43be02e066e543a36ba84cd62b63154b6b919b1459f99ea5101dedc96
+size 19885
