8c90b2e766e1168b260784bc48a9568167ea98bd
王闻浩 <841298494@qq.com>
2025-06-12 13:42:32 +0800

    Fix the error in the path selection.修复路径选择错误

--- EF_Unity/Assets/EasyFramework/Scripts/Editor/Windows/EF/PathConfig/PathConfigPanel.cs
+++ EF_Unity/Assets/EasyFramework/Scripts/Editor/Windows/EF/PathConfig/PathConfigPanel.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4745cc496026956f93aa28803d53bad4c057df734130580743f96f42d0379250
-size 7730
+oid sha256:c9fec5861996752b23f5bd1fdafb1dbcf12c8c0f05dc72b555e9f7b991008291
+size 7721
