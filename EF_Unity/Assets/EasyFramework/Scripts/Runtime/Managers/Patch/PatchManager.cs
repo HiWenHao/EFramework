6d6b1184f7bd8fc5f36e17b4edafa4ecbda3bc10
Alvin5100 <510094761@qq.com>
2025-10-23 10:13:48 +0800

    获取包体版本是不需要带时间戳。

--- EF_Unity/Assets/EasyFramework/Scripts/Runtime/Managers/Patch/PatchManager.cs
+++ EF_Unity/Assets/EasyFramework/Scripts/Runtime/Managers/Patch/PatchManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ca6ba68124d0f78e35cbd0cbe7703f3e02ad42589cc8f2a9974de9d41d01e6c
-size 16152
+oid sha256:7481017b25a96962195ec7d1f090ffa788740b98c5d50bec3417a5eb65e495e2
+size 16157
