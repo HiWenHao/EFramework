e9e51fd609ec1fe07f144d066a213b2c42145d08
Alvin8412 <841298494@qq.com>
2026-03-30 23:05:17 +0800

    删除无用设置

--- EF_Unity/Packages/com.alvin.easyframework/Editor/EFMenus/Files/ChangeScriptEncodingFormat.cs
+++ EF_Unity/Packages/com.alvin.easyframework/Editor/EFMenus/Files/ChangeScriptEncodingFormat.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08082070162f3f237990452e77a2f9b975a02fab0260174ac151eeb77188eece
-size 1696
+oid sha256:daa2fa33509ddaf5e85b538327fb47b54008c89015df4d4801aab5dfb3e03eec
+size 1695
