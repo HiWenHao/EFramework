b3b8aaa563d42fdda0c73542c09d78edb0edc2b4
Alvin5100 <510094761@qq.com>
2026-03-31 12:56:29 +0800

    暂时屏蔽语言切换

--- EF_Unity/Packages/com.alvin.easyframework/Editor/Config/Language/EF.LanguageConfig.cs
+++ EF_Unity/Packages/com.alvin.easyframework/Editor/Config/Language/EF.LanguageConfig.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d495fd458188e3d6d1a95d9cea8af474309559ac90693c550579c762cb175341
-size 5086
+oid sha256:42dd09bfe60a9edc09b6a0fc57cff3373a1ed4ff513463c417e1bb1717c7bdac
+size 5015
--- EF_Unity/Packages/com.alvin.easyframework/Editor/Windows/EF/EFProject/EFProjectPanel.cs
+++ EF_Unity/Packages/com.alvin.easyframework/Editor/Windows/EF/EFProject/EFProjectPanel.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2298d99520d59986aef06a031fff3c40a6b680a6e6e31d0a585b22e999361ff
-size 10795
+oid sha256:ccacf8ef8080d157b8c32eef7e16a39d3294a2d11aebee05ed0bff0e67687cc3
+size 10776
