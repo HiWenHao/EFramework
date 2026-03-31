6324399868be0aac3e8fe5b5b3c48c188670b737
Alvin5100 <510094761@qq.com>
2026-03-31 12:33:27 +0800

    优化作者名获取和编写

--- EF_Unity/Packages/com.alvin.easyframework/Editor/Create/CreateTemplateScript.cs
+++ EF_Unity/Packages/com.alvin.easyframework/Editor/Create/CreateTemplateScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c51c9c833bc4bde0d826fbd5a6347b7165d6e20208edfdeaece5f9cd7c935b43
-size 5444
+oid sha256:36c7b44061f51226ac0f62291eb405356d9f5db4c6a167f5909451df6329a6ab
+size 4989
--- EF_Unity/Packages/com.alvin.easyframework/Editor/Windows/EF/EFProject/EFProjectPanel.cs
+++ EF_Unity/Packages/com.alvin.easyframework/Editor/Windows/EF/EFProject/EFProjectPanel.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8f1eb3e8dc2022461645f1e8657609c75fce33f5a05d8fc26d075deaea0bd2e
-size 10673
+oid sha256:a2298d99520d59986aef06a031fff3c40a6b680a6e6e31d0a585b22e999361ff
+size 10795
