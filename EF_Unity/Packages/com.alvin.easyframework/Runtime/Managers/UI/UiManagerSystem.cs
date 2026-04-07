e4e5de9cd0fb465a9d202b6669d8d78b2494eef7
Alvin8412 <841298494@qq.com>
2026-04-07 20:50:08 +0800

    Update UiManagerSystem.cs

--- EF_Unity/Packages/com.alvin.easyframework/Runtime/Managers/UI/UiManagerSystem.cs
+++ EF_Unity/Packages/com.alvin.easyframework/Runtime/Managers/UI/UiManagerSystem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbcde6038e20657e59ecf29d8e9e9ebaaeee9a624c6edcfe2012c08debb250e4
-size 14659
+oid sha256:6db1bc4aa251f287f7d16c5d6007c8bd2b46953f63b20b33f6824e6568a5579e
+size 14657
