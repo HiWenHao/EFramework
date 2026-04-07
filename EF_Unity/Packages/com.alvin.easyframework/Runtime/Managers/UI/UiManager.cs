69849bd69a8d52d19503c639ff415fe51ba3a499
Alvin8412 <841298494@qq.com>
2026-04-07 22:07:17 +0800

    更新部分信息

--- EF_Unity/Packages/com.alvin.easyframework/Runtime/Managers/UI/UiManager.cs
+++ EF_Unity/Packages/com.alvin.easyframework/Runtime/Managers/UI/UiManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e03ccf7a4ce8b029ccdde07e2ad218a14bde464e85bfd30a2903befe8f588bd
-size 2007
+oid sha256:3dc600cede8ecff98e44723bddf450eeb6a131bb1516deccd5f3e8d3d6e10e6b
+size 2096
--- EF_Unity/Packages/com.alvin.easyframework/Runtime/Managers/UI/UiManagerSystem.cs
+++ EF_Unity/Packages/com.alvin.easyframework/Runtime/Managers/UI/UiManagerSystem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6db1bc4aa251f287f7d16c5d6007c8bd2b46953f63b20b33f6824e6568a5579e
-size 14657
+oid sha256:a8aa7ea68c4c68a611904c72419f1f5da9c30e14fa46a79081d1bf46db2a496a
+size 14669
