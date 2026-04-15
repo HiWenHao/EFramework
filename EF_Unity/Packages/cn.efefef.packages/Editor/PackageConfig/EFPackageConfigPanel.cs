d27fe034aa7f182bd25a5efc67d0e71a49b34915
Alvin5100 <510094761@qq.com>
2026-04-16 05:24:19 +0800

    增加Git Token的设置

--- EF_Unity/Packages/cn.efefef.packages/Editor/PackageConfig/EFPackageConfigPanel.cs
+++ EF_Unity/Packages/cn.efefef.packages/Editor/PackageConfig/EFPackageConfigPanel.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25e064d44088f19e0416f060ec555cfa313e788a7482f1670e35ac192fc4fb27
-size 21021
+oid sha256:c230203426fa18db9aa3d2e11c4a87aa6c7cddbd36788463abd43278393d1f9b
+size 21801
--- EF_Unity/Packages/cn.efefef.packages/Editor/PackageConfig/JsonData/PackageConfig.cs
+++ EF_Unity/Packages/cn.efefef.packages/Editor/PackageConfig/JsonData/PackageConfig.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d623d08a47f087f981d5aa46e5b1324bd476aa96d84f32f838a43d57da20447
-size 903
+oid sha256:f61ede246ef9c49119c9e742313a1fe7cf5d18ca9555f20125d69fb0f3a032dc
+size 1019
