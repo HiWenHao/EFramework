45477299b3225980bf84ff49f55904d7bee99a22
Alvin5100 <510094761@qq.com>
2026-04-16 04:14:37 +0800

    增加判空处理

--- EF_Unity/Packages/cn.efefef.core/Editor/Windows/CustomProgressWindow.cs
+++ EF_Unity/Packages/cn.efefef.core/Editor/Windows/CustomProgressWindow.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae629dcf9060c558d6a1ff404c9987e4150b2cf730eea7ebc906a0aaf568c8a1
-size 4317
+oid sha256:eafb235ec26bbb41bfe7ff31a2cb47db7d41b06550b80ff061a4d56966809a36
+size 4395
--- EF_Unity/Packages/cn.efefef.packages/Editor/PackageConfig/EFPackageConfigPanel.cs
+++ EF_Unity/Packages/cn.efefef.packages/Editor/PackageConfig/EFPackageConfigPanel.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f70491b14f2fa1c0e1ca799e75403e4464a968a83e49a118e299e9d0fd5871c9
-size 20345
+oid sha256:a0b9461b50387850b011cebe90c6ab0dd1481d6771eae39002c1842760eb584b
+size 20588
