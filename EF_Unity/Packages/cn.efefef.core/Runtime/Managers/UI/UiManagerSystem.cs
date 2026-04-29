055f9d3edc7a9dd3b69a4998694e6b5ad5b9ae97
Alvin5100 <510094761@qq.com>
2026-04-29 10:37:56 +0800

    [ UI ] 修复错误传递

--- EF_Unity/Packages/cn.efefef.core/Runtime/Managers/UI/UiManagerSystem.cs
+++ EF_Unity/Packages/cn.efefef.core/Runtime/Managers/UI/UiManagerSystem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0273d74a81e5a8b220abca6aea6c38922a935abc8a9855738bd0a72c973e248
-size 17583
+oid sha256:440e83adbb69646e7eff9362d1529e2784387a76fce0d890e7d5570438d0b278
+size 17577
