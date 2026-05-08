142b5374fca4fbef69dd2ffc3e5f1d54955bb9c8
Alvin8412 <841298494@qq.com>
2026-05-08 21:57:33 +0800

    [ Audio ] 初步增加通过音频切片播放背景音乐

--- EF_Unity/Packages/cn.efefef.core/Runtime/EF.Start.cs
+++ EF_Unity/Packages/cn.efefef.core/Runtime/EF.Start.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14b4109e2a1b76decf019de94342f0dbe717321654ba77bb080d815ab37a5645
-size 5217
+oid sha256:e6febecb24809ae9680a4b3bc54d0938cc919560f07adc15a4fc4c4c6405c1ca
+size 5179
--- EF_Unity/Packages/cn.efefef.core/Runtime/Managers/Assets/AssetsRootManager.cs
+++ EF_Unity/Packages/cn.efefef.core/Runtime/Managers/Assets/AssetsRootManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b037b5a28c962e21e9bdc6c7cd6b0b4c04cc07f9b9e45cb604e5ab78a5c664a4
-size 7453
+oid sha256:21adb6aa078beea34596e01c706305e68b1d8af395241d6f12059b76accb5365
+size 7511
