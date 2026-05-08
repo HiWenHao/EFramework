c93a71c371d072cf122b8eb2872701aeaf7d0ad7
Alvin5100 <510094761@qq.com>
2026-05-08 16:34:29 +0800

    [ UI ] 美化自动绑定面板

--- EF_Unity/Packages/cn.efefef.ui/Editor/Windows/UiBindingConfig/UiBindingEditor.cs
+++ EF_Unity/Packages/cn.efefef.ui/Editor/Windows/UiBindingConfig/UiBindingEditor.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5db11d6279990ebe4509ef0e43526033c1c93c12917c0d95b5dc4c5bc416f81a
-size 30370
+oid sha256:2c0d7220eb49de5c6acda7e17019b74933fa2b24190cc3864f96d0d5b7f8eabd
+size 30382
