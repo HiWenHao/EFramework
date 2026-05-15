c18290bbdca3ddbb1447f8f52ac110e2b5200425
Alvin5100 <510094761@qq.com>
2026-05-15 19:31:27 +0800

    [ Core ] 增加系统和系统拥有者接口

---
+++ EF_Unity/Packages/cn.efefef.core/Runtime/Framework/Inferface/ISystem.cs
@@ -0,0 +1,21 @@
+/*
+ * ================================================
+ * Describe:      系统接口，主要给实现ISystemOwner接口的类型注入自身
+ * Author:        Alvin5100
+ * CreationTime:  2026-05-15 18:23:14
+ * ModifyAuthor:  Alvin5100
+ * ModifyTime:    2026-05-15 18:23:14
+ * ScriptVersion: 0.1
+ * ===============================================
+ */
+
+namespace EasyFramework
+{
+    /// <summary>
+    /// 系统接口，主要给实现ISystemOwner接口的类型注入自身
+    /// <para>System interface, mainly for injecting itself into the types that implement the ISystemOwner interface.</para>
+    /// </summary>
+    public interface ISystem
+    {
+    }
+}
