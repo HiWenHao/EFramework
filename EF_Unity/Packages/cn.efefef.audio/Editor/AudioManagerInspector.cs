90c0921cf395ddfb8f8cdb9699518989eec7930d
Alvin5100 <510094761@qq.com>
2026-05-18 17:46:50 +0800

    [ Audio] 删除无用程序集引用

--- EF_Unity/Packages/cn.efefef.audio/Editor/AudioManagerInspector.cs
+++ EF_Unity/Packages/cn.efefef.audio/Editor/AudioManagerInspector.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67ddf939bed20e0fe61f14122bd68f919090a4b50c03c23ec71f5bfa85d688c0
-size 5732
+oid sha256:2313bce1a9e63bdab81b39453011d6555da13020edd6857bded6895d85b5bd75
+size 4624
