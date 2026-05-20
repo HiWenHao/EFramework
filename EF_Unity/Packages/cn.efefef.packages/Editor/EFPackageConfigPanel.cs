54848e6ca983c5b2d9778d25772265504403ae82
Alvin5100 <510094761@qq.com>
2026-05-20 19:15:22 +0800

    [ Packages] 修复下载和删除后的对于数据同步

--- EF_Unity/Packages/cn.efefef.packages/Editor/EFPackageConfigPanel.cs
+++ EF_Unity/Packages/cn.efefef.packages/Editor/EFPackageConfigPanel.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb93327145edf7cc12184d61b057e4964bce4acc49d0d4cfc7b0bd12f1d59715
-size 18908
+oid sha256:0436978192761d3e21e4eda1adde6a10e8ff3ea5ffb904fadb7183d29a96be55
+size 19156
