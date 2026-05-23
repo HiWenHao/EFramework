52d7d0cadf57f86b4a8d872caff73a0097eaf8f8
Alvin5100 <510094761@qq.com>
2026-05-23 17:07:27 +0800

    [ Core ] 增加对象基类、 引用对象基类

---
+++ EF_Unity/Packages/cn.efefef.core/Runtime/Framework/Base/Objects/EFObject.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:65bc1062a4b9833f086a92c6eeb485156df5ad833ba80ab724efcd161642c6e7
+size 485
---
+++ EF_Unity/Packages/cn.efefef.core/Runtime/Framework/Base/Objects/ReferenceObject.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b25f6ed4dd451801bd81b5d4b2fc841c94909c3aacdd8135c3299ce896fcc035
+size 1833
