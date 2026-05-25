519b03ca68e9a62b21d6fda4a05cb5ac807bd58c
Alvin5100 <510094761@qq.com>
2026-05-25 18:51:19 +0800

    [ UI ] 删除无用字段

--- EF_Unity/Packages/cn.efefef.ui/Editor/Windows/UiBindingConfig/EFUiBindingConfigPanel.cs
+++ EF_Unity/Packages/cn.efefef.ui/Editor/Windows/UiBindingConfig/EFUiBindingConfigPanel.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:901985c5adf0e5b5377e6d6e296c1af4afa2ed834564531caf8622f255313b9a
-size 2044
+oid sha256:afd75d8b156b1e7aabb418b8063fd0be27797594cd0c577084a2914e9b388633
+size 1703
