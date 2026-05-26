a695c961d6920770e5e5f47363306817a455badd
Alvin5100 <510094761@qq.com>
2026-05-26 13:58:56 +0800

    [ Core ] 删除无用字段

--- EF_Unity/Packages/cn.efefef.core/Editor/Windows/AssetRenameWindow/AssetRenameWindow.cs
+++ EF_Unity/Packages/cn.efefef.core/Editor/Windows/AssetRenameWindow/AssetRenameWindow.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ae815f9be8b7315b01c04b99910e47f0fe029cf713d41c5faa65f5a455034eb
-size 17658
+oid sha256:78ba618e9847d71a0f3213f9d94c4ba21af003d69badd891ac8ecc19bbcfbd9a
+size 17180
