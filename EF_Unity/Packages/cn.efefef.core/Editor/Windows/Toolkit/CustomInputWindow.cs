b0bb2a9127c0c834876c9441e490749e3170b83a
Alvin5100 <510094761@qq.com>
2026-05-30 15:27:37 +0800

    [ Core ] 优化窗口基类和窗口工具

--- EF_Unity/Packages/cn.efefef.core/Editor/Windows/Toolkit/CustomInputWindow.cs
+++ EF_Unity/Packages/cn.efefef.core/Editor/Windows/Toolkit/CustomInputWindow.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0dd4603905af08dee810e24f45fc58b4979afff88024b05da5553d1496fbad4e
-size 6939
+oid sha256:dad98d5c82939d6467f20f4fc148ae3303ebf125ccdd47041d770fc4d3c46310
+size 6622
