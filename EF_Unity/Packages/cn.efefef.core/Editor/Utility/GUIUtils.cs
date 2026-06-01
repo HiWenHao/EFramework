6d22caac56db8390a094ba9645627dc503d58acc
Alvin5100 <510094761@qq.com>
2026-06-01 17:51:57 +0800

    [ Core ] 优化脚本查询工具

--- EF_Unity/Packages/cn.efefef.core/Editor/Utility/GUIUtils.cs
+++ EF_Unity/Packages/cn.efefef.core/Editor/Utility/GUIUtils.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33c3ecd306b0866c6097d842d9b5d7d9733931c80e92aa326e3bfd3728f5ad84
-size 8599
+oid sha256:f01104f70508fec642ee4121a12718d1e08d283ddb1ca1c7560ceab809047d46
+size 9521
--- EF_Unity/Packages/cn.efefef.core/Editor/Windows/ScriptTools/ScriptToolsWindow.cs
+++ EF_Unity/Packages/cn.efefef.core/Editor/Windows/ScriptTools/ScriptToolsWindow.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3711c1a304a3ef574836a79f3c4de1e697ca4e5f431f42be1cb4ace556d26e98
-size 11017
+oid sha256:5656f66341026d3d1a936f0833562efb1825a4920a8be4bb3c9f221589920db5
+size 10560
