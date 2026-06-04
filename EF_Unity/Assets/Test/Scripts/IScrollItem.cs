7c1d086be593d113ad5218be601e4484ef9810c3
Alvin5100 <510094761@qq.com>
2026-06-04 15:01:13 +0800

    完善案例工程

--- EF_Unity/Assets/Test/Scripts/IScrollItem.cs
+++ EF_Unity/Assets/Test/Scripts/IScrollItem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b01ace5a94d53046fee2af704d65ad8a4aae4ce719c7fa6fb04a068ac68420cb
-size 1135
+oid sha256:c7b23b533324ac9b5a96a4c21c4d125cf65401c621729cbc16d68c8a280f5a36
+size 1764
--- EF_Unity/Assets/Test/Scripts/ScrollItemBase.cs
+++ EF_Unity/Assets/Test/Scripts/ScrollItemBase.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:066cf01bd8002385518ded35f2fbc643e6f12e65dc5cd473bef596e0f4a3cd32
-size 3001
+oid sha256:6c72ebf38ce05527ab97c04642e8556797bc60d2b8741f0356015c5dc7c3c0ee
+size 4071
