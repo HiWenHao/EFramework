acbb453e45482f448c43c677dcf6a52ffc697e6e
Alvin5100 <510094761@qq.com>
2026-06-04 17:53:03 +0800

    [ Core ] 完善HeaderPro的显示

--- EF_Unity/Assets/Test/Scripts/InfiniteIrregularScrollList.cs
+++ EF_Unity/Assets/Test/Scripts/InfiniteIrregularScrollList.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f28781d92afffe4f21b97e57bd4a8bf491d13355ca5dfd6cdd5e281e5421931c
-size 65964
+oid sha256:75c497a26348eab1543814d2791b662385145e622683dab7fa991e3b508a3faa
+size 65557
--- EF_Unity/Packages/cn.efefef.core/Editor/InspectorExpand/HeaderProDrawer.cs
+++ EF_Unity/Packages/cn.efefef.core/Editor/InspectorExpand/HeaderProDrawer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:386dbe916fe75179f2eccbdd609c9d9dc8712b0046c1749151bc5e06574193e5
-size 1245
+oid sha256:090d8ed3055b7a8d54d92408d070105b9c1a96e34bedfcd56c14ace17f100424
+size 1249
