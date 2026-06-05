79eff0bf0ceaffe99f8ce3790a9ed333c4af1a46
Alvin5100 <510094761@qq.com>
2026-06-05 18:04:29 +0800

    增加测试案例

---
+++ EF_Unity/Assets/Test/CircularScrollListProPanel.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:034c019533f4f70843e059073cc636b61603271f4e0ce1839f6a29a9b5237929
+size 2990
---
+++ EF_Unity/Packages/cn.efefef.ui/Runtime/UI/ScrollRectPro/CircularScrollListPro.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:178874f964e83c2f061b02e59d4c0f7fd29233c3d233dc4804d021c67099ce29
+size 19888
