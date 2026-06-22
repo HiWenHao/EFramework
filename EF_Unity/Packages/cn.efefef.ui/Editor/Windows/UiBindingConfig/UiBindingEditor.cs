add981d0c56f337c1e8a6c9f0dad8a57a5f7d5ec
Alvin5100 <510094761@qq.com>
2026-06-22 14:51:28 +0800

    [  UI ] 优化

--- EF_Unity/Packages/cn.efefef.ui/Editor/Windows/UiBindingConfig/UiBindingEditor.cs
+++ EF_Unity/Packages/cn.efefef.ui/Editor/Windows/UiBindingConfig/UiBindingEditor.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ba68bcb98c5c9d1ba5314ae615355da2672ae4a2b7c09dda3487688db12e1ff
-size 30482
+oid sha256:d9abc8089c61c39a2dc5e90909a7f18f614414553394d5b9bf1cd39f560d8963
+size 30478
