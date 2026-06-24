65beb50c250756d3b3045384d10f163d6a23059f
Alvin5100 <510094761@qq.com>
2026-06-24 17:12:06 +0800

    [ UI ] 优化图集收集器

--- EF_Unity/Packages/cn.efefef.ui/Editor/Config/SpriteCollection.cs
+++ EF_Unity/Packages/cn.efefef.ui/Editor/Config/SpriteCollection.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b68c567db38ebeefe33cbad32604a54f92a6270fa5841becfff7778750c76dca
-size 1802
+oid sha256:93818dc989f0b903fe53f60f76d8489b79790308b2ddb614560359c74f41249a
+size 1019
--- EF_Unity/Packages/cn.efefef.ui/Editor/Config/SpriteCollectionEdit.cs
+++ EF_Unity/Packages/cn.efefef.ui/Editor/Config/SpriteCollectionEdit.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b287f68cdebeebd68d7d44eceeffb818c1094152ee93f94df8cf61c2919f08b
-size 16553
+oid sha256:f2c5fa05723ba3221b8172a2ad4790b44d550c833ef1014562d6411395f6121f
+size 10902
