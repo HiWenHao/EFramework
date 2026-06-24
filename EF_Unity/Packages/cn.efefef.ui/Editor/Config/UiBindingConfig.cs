7c870662474868f50cd06dc25931b8958056711f
Alvin5100 <510094761@qq.com>
2026-06-24 18:48:25 +0800

    [ UI ] 更改部分组件缩略名

--- EF_Unity/Packages/cn.efefef.ui/Editor/Config/UiBindingConfig.cs
+++ EF_Unity/Packages/cn.efefef.ui/Editor/Config/UiBindingConfig.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3da39606037271f613da04b52fd96229cf0383acf78ddd68e779cb28623618a
-size 3005
+oid sha256:6670ee386c129c09b3a7d660e962cd6cc32468660eb91f5ba4de9280666a2ea3
+size 3002
