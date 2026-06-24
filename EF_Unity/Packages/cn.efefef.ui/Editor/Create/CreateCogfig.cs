f51d10183d27194e523060438d1dc540833b570f
Alvin5100 <510094761@qq.com>
2026-06-24 18:08:29 +0800

    [ UI ] 更改配置文件的创建优先级

--- EF_Unity/Packages/cn.efefef.ui/Editor/Create/CreateCogfig.cs
+++ EF_Unity/Packages/cn.efefef.ui/Editor/Create/CreateCogfig.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f917b4b4f9b43598bf9701af10cefd083997b9184e98e2f4130494cd31eaaf2
+oid sha256:b3a2879bc299b38e4b3cf161d7cdfeac0a469295b52cefae228919ebd94442bd
 size 1204
