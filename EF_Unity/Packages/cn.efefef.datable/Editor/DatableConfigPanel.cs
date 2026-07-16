817fad3d31b6866a33aa4327467dac670bd30a0c
Alvin5100 <510094761@qq.com>
2026-07-16 15:55:15 +0800

    [ Datable ] 美化显示

--- EF_Unity/Packages/cn.efefef.datable/Editor/DatableConfigPanel.cs
+++ EF_Unity/Packages/cn.efefef.datable/Editor/DatableConfigPanel.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afe130ab015863fc5be577b11b54622575487868e4469d2854262f7193e661ec
-size 19182
+oid sha256:a29716d036d7e38d93b792fc2a60de8531cbe68d5b81e1e907f53f3b061ba7ac
+size 19200
